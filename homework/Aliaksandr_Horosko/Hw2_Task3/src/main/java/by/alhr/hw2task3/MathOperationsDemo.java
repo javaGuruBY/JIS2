c90734572538f5468cf240f8ffd290170c19181f
Alexhorosko <zega_boll@mail.ru>
2020-03-14 19:13:02 +0300

    Hw2_Task3 mathOperations

---
+++ homework/Aliaksandr_Horosko/Hw2_Task3/src/main/java/by/alhr/hw2task3/MathOperationsDemo.java
@@ -0,0 +1,13 @@
+package by.alhr.hw2task3;
+
+import by.alhr.hw2task3.bean.MathOperations;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        MathOperations mathOperations = new MathOperations();
+        System.out.println(mathOperations.summ());
+        System.out.println(mathOperations.difference());
+        System.out.println(mathOperations.multiplication());
+        System.out.println(mathOperations.division());
+    }
+}
---
+++ homework/Aliaksandr_Horosko/Hw2_Task3/src/main/java/by/alhr/hw2task3/bean/MathOperations.java
@@ -0,0 +1,25 @@
+package by.alhr.hw2task3.bean;
+
+import java.util.Objects;
+
+public class MathOperations {
+    int a = 10;
+    int b = 10;
+    public double summ() {
+
+        return a + b;
+    }
+    public double difference () {
+
+        return a - b;
+    }
+    public double multiplication () {
+
+        return a * b;
+    }
+    public double division() {
+
+        return a / b;
+    }
+
+}
