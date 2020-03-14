4713c9770413f05a6201c908441513994f2dd7d9
SerGamEsNIUS <chemist-sergeev98@yandex.ru>
2020-03-14 16:23:00 +0300

    Eugene_Sergeev_Hw2Task3

---
+++ homework/Eugene_Sergeev/Hw2Task3/src/main/java/by/serg/bean/MathOperationsDemo.java
@@ -0,0 +1,11 @@
+package by.serg.bean;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        MathOperations mathOperations = new MathOperations();
+        System.out.println(mathOperations.differenceOfNumbers(228, 159));
+        System.out.println(mathOperations.summOfNumbers(35, 34));
+        System.out.println(mathOperations.multiplicationOfNumbers(23, 3));
+        System.out.println(mathOperations.divisionOfNumbers(345, 5));
+    }
+}
