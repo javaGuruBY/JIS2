630249a9511479bbc6efd4ff4b9c9b42d991c3dd
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-03-14 23:43:34 +0300

    add my Hw2Task3

---
+++ homework/Evgeny_Korovin/by/evgKor/Main.java
@@ -0,0 +1,11 @@
+package by.evgKor;
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperationDemo result=new MathOperationDemo();
+        System.out.println("result of amount= "+result.amount(30,27));
+        System.out.println("result of difference= "+result.difference(560,1020));
+        System.out.println("result of multiplication= "+result.multiplication(60,60));
+        System.out.println("result of amount= "+result.division(40,6));
+    }
+}
