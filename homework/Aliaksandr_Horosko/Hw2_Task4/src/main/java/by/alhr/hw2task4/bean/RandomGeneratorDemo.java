899584fa4e5fa7ea89714d0603ae9fa96fc081cd
Alexhorosko <zega_boll@mail.ru>
2020-03-14 20:16:14 +0300

    Hw2_Task4

---
+++ homework/Aliaksandr_Horosko/Hw2_Task4/src/main/java/by/alhr/hw2task4/bean/RandomGeneratorDemo.java
@@ -0,0 +1,11 @@
+package by.alhr.hw2task4.bean;
+
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+        RandomGenerator randomGenerator = new RandomGenerator();
+        System.out.println(randomGenerator.aDouble());
+        System.out.println(randomGenerator.bDouble());
+        System.out.println(randomGenerator.cDouble());
+        System.out.println(randomGenerator.Random());
+    }
+}
