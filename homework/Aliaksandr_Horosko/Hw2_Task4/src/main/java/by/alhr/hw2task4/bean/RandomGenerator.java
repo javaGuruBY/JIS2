25e8086df95d5515fec45963e54e6d063bf74911
Alexhorosko <zega_boll@mail.ru>
2020-03-15 23:25:35 +0300

    corrected Hw2_Task4

--- homework/Aliaksandr_Horosko/Hw2_Task4/src/main/java/by/alhr/hw2task4/bean/RandomGenerator.java
+++ homework/Aliaksandr_Horosko/Hw2_Task4/src/main/java/by/alhr/hw2task4/bean/RandomGenerator.java
@@ -1,80 +1,14 @@
 package by.alhr.hw2task4.bean;
 
-import java.util.Objects;
-
 public class RandomGenerator {
-    public RandomGenerator() {
-        return;
-    }
 
-    @Override
-    public String toString() {
-        return "RandomGenerator{" +
-                "a=" + a +
-                ", b=" + b +
-                ", c=" + c +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RandomGenerator that = (RandomGenerator) o;
-        return Double.compare(that.a, a) == 0 &&
-                Double.compare(that.b, b) == 0 &&
-                Double.compare(that.c, c) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(a, b, c);
-    }
-
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public double getC() {
-        return c;
-    }
-
-    public void setC(double c) {
-        this.c = c;
-    }
-
-    public RandomGenerator(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    double a = Math.random() * 3;
+    double a = Math.random() * 5;
     double b = Math.random() * 8;
     double c = Math.random() * 12;
-    public double aDouble (){
-        return a;
-    }
-    public double bDouble (){
-        return b;
-    }
-    public double cDouble (){
-        return c;
-    }
-    public double Random (){
+
+    public double amountRandom() {
         return a + b + c;
     }
-
 }
+
+
--- homework/Aliaksandr_Horosko/Hw2_Task4/src/main/java/by/alhr/hw2task4/bean/RandomGeneratorDemo.java
+++ homework/Aliaksandr_Horosko/Hw2_Task4/src/main/java/by/alhr/hw2task4/bean/RandomGeneratorDemo.java
@@ -3,9 +3,9 @@ package by.alhr.hw2task4.bean;
 public class RandomGeneratorDemo {
     public static void main(String[] args) {
         RandomGenerator randomGenerator = new RandomGenerator();
-        System.out.println(randomGenerator.aDouble());
-        System.out.println(randomGenerator.bDouble());
-        System.out.println(randomGenerator.cDouble());
-        System.out.println(randomGenerator.Random());
+        System.out.println("a = " + randomGenerator.a);
+        System.out.println("b = " + randomGenerator.b);
+        System.out.println("c = " + randomGenerator.c);
+        System.out.println("amountRandom = " + randomGenerator.amountRandom());
     }
 }
