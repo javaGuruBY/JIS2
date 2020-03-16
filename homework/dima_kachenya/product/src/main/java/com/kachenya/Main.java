ee6f7d41e55233a3ddfa874741a1e26ea084cd49
Dima <53133762+DimaKachenya@users.noreply.github.com>
2020-03-16 12:58:52 +0300

    Add files via upload

---
+++ homework/dima_kachenya/product/src/main/java/com/kachenya/Main.java
@@ -0,0 +1,11 @@
+package com.kachenya;
+
+import com.kachenya.service.ServiceForProduct;
+
+public class Main {
+    public static void main(String[] args) {
+        ServiceForProduct milk=new ServiceForProduct("Milk",0.8d,0.2d);
+        milk.printIfo();
+
+    }
+}
