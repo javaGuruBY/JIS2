71d25932b0d6a71890686b341d1e7763c8431b76
Dima <53133762+DimaKachenya@users.noreply.github.com>
2020-03-24 15:05:02 +0300

    Add files via upload

---
+++ homework/dima_kachenya/primeNumber/src/main/java/com/kachenya/Main.java
@@ -0,0 +1,9 @@
+package com.kachenya;
+
+import com.kachenya.primenumbers.PrimeNumbers;
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeNumbers.prineNumbers();
+    }
+}
