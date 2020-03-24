eeb0b57ee1a1183e2fd1dea822ce8f99dfbc9826
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-03-24 15:32:43 +0300

    add Hw6Task1

---
+++ homework/src/main/java/by/evgKor/ForOddChecker.java
@@ -0,0 +1,13 @@
+package by.evgKor;
+
+public class ForOddChecker {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println("i= " + i);
+            }
+        }
+    }
+}
+
