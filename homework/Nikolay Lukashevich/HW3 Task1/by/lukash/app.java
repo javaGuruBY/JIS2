31ef916a453222872ccc93eaeb26e8f43d0e0e3a
Nikolay Lukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-04-02 19:37:06 +0300

    Update app.java

--- homework/Nikolay Lukashevich/HW3 Task1/by/lukash/app.java
+++ homework/Nikolay Lukashevich/HW3 Task1/by/lukash/app.java
@@ -4,11 +4,11 @@ import by.lukash.services.DogServices;
 
 public class app {
     public static void main (String[] args){
-        Dog Dog = new Dog (5, "black", "Bobik");
-        System.out.println("hello, my name is "+Dog.getName()+" I'm "+Dog.getAge()+" years old "+" I'm a "+Dog.getColor()+" color");
+        Dog dog = new Dog (5, "black", "Bobik");
+        System.out.println("hello, my name is "+dog.getName()+" I'm "+dog.getAge()+" years old "+" I'm a "+dog.getColor()+" color");
         DogServices dogService=new DogServices();
         dogService.eat();
-        dogService.sleep(firstDog);
+        dogService.sleep(dog);
         dogService.voice();
 
 
