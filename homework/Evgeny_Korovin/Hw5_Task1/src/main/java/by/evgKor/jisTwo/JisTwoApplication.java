cc9e751f7a921a19fdd9c5dbca4e3b73b5fda93f
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-04-03 15:52:51 +0300

    corrected hw5_task1

--- homework/Evgeny_Korovin/Hw5_Task1/src/main/java/by/evgKor/jisTwo/JisTwoApplication.java
+++ homework/Evgeny_Korovin/Hw5_Task1/src/main/java/by/evgKor/jisTwo/JisTwoApplication.java
@@ -1,13 +1,10 @@
 package by.evgKor.jisTwo;
 
-import by.evgKor.jisTwo.bean.Counter;
 import by.evgKor.jisTwo.service.CounterService;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JisTwoApplication {
-
 	public static void main(String[] args) {
 		Thread thread=new Thread(new CounterService());
 		thread.start();
