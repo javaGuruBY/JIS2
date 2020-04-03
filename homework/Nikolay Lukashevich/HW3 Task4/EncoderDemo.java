e61945947f6c39741f8973d4e4320082ed3b1a71
Nikolay Lukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-04-03 17:53:17 +0300

    made changes according to comments

--- homework/Nikolay Lukashevich/HW3 Task4/EncoderDemo.java
+++ homework/Nikolay Lukashevich/HW3 Task4/EncoderDemo.java
@@ -3,9 +3,9 @@ package by.nluaksh;
 public class EncoderDemo {
     public static void main(String[] args) {
 
-        short Short = 65;
-        char Char = 'X';
-        Encoder.encode(Short);
-        Encoder.decode(Char);
+        short myShort = 65;
+        char myChar = 'X';
+        Encoder.encode(myShort);
+        Encoder.decode(myChar);
     }
 }
