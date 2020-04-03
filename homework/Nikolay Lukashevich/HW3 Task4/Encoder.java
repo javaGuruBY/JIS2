228d0bdfa1de42826685db887a95940530cd7798
Nikolay Lukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-04-03 18:35:05 +0300

    Made changes according to comments

--- homework/Nikolay Lukashevich/HW3 Task4/Encoder.java
+++ homework/Nikolay Lukashevich/HW3 Task4/Encoder.java
@@ -1,6 +1,8 @@
 package by.nlukash;
 
 public class Encoder {
+       private Encoder(){
+    }
     public static void encode(short code) {
         System.out.println(code + " =>" + (char) code);
     }
