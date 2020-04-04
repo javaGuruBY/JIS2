486e5043a29cf94f22850ef24eddbab5d6477e39
SevenSemmy <61471229+SevenSemmy@users.noreply.github.com>
2020-04-04 10:17:00 +0300

    Update hw2_task4.java

--- homework/Alexandr_Savenya/hw2_task4.java
+++ homework/Alexandr_Savenya/hw2_task4.java
@@ -1,17 +1,16 @@
 package by.seven;
 
-import java.util.Random;
-
 public class RandomGeneratorDemo {
     public static void main(String[] args) {
-        Random random = new Random();
-        int firstNumber = random.nextInt();
-        int secondNumber = random.nextInt();
-        int thirdNumber = random.nextInt();
-        System.out.println("First generaited number: " + firstNumber);
-        System.out.println("Second generaited number: " + secondNumber);
-        System.out.println("Third generaited number: " + thirdNumber);
-        long sum = firstNumber + secondNumber + thirdNumber;
+     int a = 0; //нижнее значение диапазона
+     int b = 100; // верхнее значение диапазона
+        int firstRandomNum = a + (int) (Math.random() * b);
+        int secondRandomNum = a + (int) (Math.random() * b);
+        int thirdRandomNum = a + (int) (Math.random() * b);
+        System.out.println("First generaited number: " + firstRandomNum);
+        System.out.println("Second generaited number: " + secondRandomNum);
+        System.out.println("Third generaited number: " + thirdRandomNum);
+        long sum = firstRandomNum + secondRandomNum + thirdRandomNum;
         System.out.println("Sum: " + sum);
     }
 }
