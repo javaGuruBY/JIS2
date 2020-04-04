90765d1b6276fe801ac82e15de48fe53b324f421
Alexhorosko <zega_boll@mail.ru>
2020-04-05 00:34:04 +0300

    Hw3_Task4_Encoder !!!!!!LAST VERSION!!!!!!!!!!!

--- homework/Aliaksandr_Horosko/Hw3_Task4/src/java/by/alhr/EncoderDemo.java
+++ homework/Aliaksandr_Horosko/Hw3_Task4/src/java/by/alhr/EncoderDemo.java
@@ -5,11 +5,10 @@ import by.alhr.service.EncoderService;
 
 public class EncoderDemo {
     public static void main(String[] args) {
-        Encoder encoder = new Encoder((short) 65,'A');
-        Encoder encoder1 = new Encoder((short) 88, 'X');
+        Encoder encoder = new Encoder((short) 65,'X');
         EncoderService encoderService = new EncoderService();
         encoderService.encode(encoder);
-        encoderService.decode(encoder1);
+        encoderService.decode(encoder);
 
 
     }
--- homework/Aliaksandr_Horosko/Hw3_Task4/src/java/by/alhr/service/EncoderService.java
+++ homework/Aliaksandr_Horosko/Hw3_Task4/src/java/by/alhr/service/EncoderService.java
@@ -4,14 +4,11 @@ import by.alhr.bean.Encoder;
 
 public class EncoderService {
 
-    public EncoderService() {
-    }
-
     public void encode(Encoder encoder) {
         System.out.println(encoder.getCode() + " => " + (char) encoder.getCode());
     }
 
     public void decode(Encoder encoder) {
-        System.out.println(encoder.getSymbol() + " => " + (short) encoder.getSymbol());
+        System.out.println(encoder.getSymbol() + " => " + (short)encoder.getSymbol());
     }
 }
