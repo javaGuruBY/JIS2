22b23f056af344b22a53fb827d0fbfcc296c95be
Alexhorosko <zega_boll@mail.ru>
2020-04-04 20:38:14 +0300

    Hw3_Task1_Dog fixed homework!

--- homework/Aliaksandr_Horosko/Hw3_Task1/src/java/by/alhr/DogDemo.java
+++ homework/Aliaksandr_Horosko/Hw3_Task1/src/java/by/alhr/DogDemo.java
@@ -5,10 +5,16 @@ import by.alhr.service.DogService;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog nameDog = new Dog (8, "red", "Frayzi");
+        Dog frayzi = new Dog(8, "red", "Frayzi");
+        Dog dodik = new Dog(5, "black", "Dodik");
+        Dog klyacha = new Dog(12, "gray", "Klyacha");
         DogService dogService = new DogService();
-        dogService.voice();
-        dogService.eat();
-        dogService.sleep();
+        System.out.println("Frayzi: ");
+        dogService.eat(frayzi);
+        dogService.sleep(frayzi);
+        System.out.println("Dodik: ");
+        dogService.voice(dodik);
+        System.out.println("Klyacha: ");
+        dogService.eat(klyacha);
     }
 }
