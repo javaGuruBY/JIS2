bc1ab0320422f19ebb08df4b6349b39531396e0d
Alexhorosko <zega_boll@mail.ru>
2020-04-04 19:52:40 +0300

    Hw3_Task3_Circle fixed homework!

--- homework/Aliaksandr_Horosko/Hw3_Task3/src/java/by/alhr/CircleDemo.java
+++ homework/Aliaksandr_Horosko/Hw3_Task3/src/java/by/alhr/CircleDemo.java
@@ -5,7 +5,7 @@ import by.alhr.service.CircleService;
 
 public class CircleDemo {
     public static void main(String[] args) {
-        Circle circle = new Circle(25);
+        Circle circle = new Circle(25.13);
         Circle circle2 = new Circle(5.25);
         Circle circle3 = new Circle(7);
 
