4f2680f2e106dfa270d469f0712c21b091228786
AcyaKi <anastasia.kichaeva.22@gmail.com>
2020-04-04 23:20:00 +0300

    make current speed = target or max speed, or zero depending on the condition.

--- homework/Anastasia_Kichaeva/Hw6_Task4/src/main/java/acya/ki/service/CarService.java
+++ homework/Anastasia_Kichaeva/Hw6_Task4/src/main/java/acya/ki/service/CarService.java
@@ -19,10 +19,12 @@ public class CarService implements Runnable{
             System.out.println("Current speed is equal or more than target speed or max speed!");
         } else if (carBean.getCurrentSpeed() < targetSpeed && targetSpeed <= carBean.getMaxSpeed()) {
             for (int i = carBean.getCurrentSpeed() + 1; i <= targetSpeed; i++) {
+                carBean.setCurrentSpeed(targetSpeed);
                 System.out.println("Accelerate to target speed: " + i + " klm.");
             }
         } else if (carBean.getCurrentSpeed() < targetSpeed && targetSpeed > carBean.getMaxSpeed()) {
             for (int i = carBean.getCurrentSpeed() + 1; i <= carBean.getMaxSpeed(); i++) {
+                carBean.setCurrentSpeed(carBean.getMaxSpeed());
                 System.out.println("Target speed more than max speed, therefore accelerate to max speed: " + i + " klm.");
             }
         }
@@ -34,10 +36,12 @@ public class CarService implements Runnable{
             System.out.println("Current speed is equal or less than target speed or 0!");
         } else if (carBean.getCurrentSpeed() >= targetSpeed && targetSpeed > 0) {
             for (int i = carBean.getCurrentSpeed() - 1; i >= targetSpeed; i--) {
+                carBean.setCurrentSpeed(targetSpeed);
                 System.out.println("Decelerate to target speed: " + i + " klm.");
             }
         } else if (carBean.getCurrentSpeed() >= targetSpeed && targetSpeed <= 0) {
             for (int i = carBean.getCurrentSpeed() - 1; i >= 0; i--) {
+                carBean.setCurrentSpeed(0);
                 System.out.println("Target speed less than or equal to 0, therefore decelerate to zero: " + i + " klm.");
             }
         }
@@ -45,7 +49,7 @@ public class CarService implements Runnable{
 
     public boolean isDriving(){
         if (carBean.getCurrentSpeed() <= 0) {
-        System.out.println("Current speed equals or less than 0 klm. Car is stopped!");
+            System.out.println("Current speed equals or less than 0 klm. Car is stopped!");
             return false;
         } return true;
     }
@@ -69,10 +73,16 @@ public class CarService implements Runnable{
 
     @Override
     public void run() {
-        CarBean carBean1 = new CarBean("BMW", "Black", 400);
-        carBean1.setCurrentSpeed(314);
+        CarBean carBean1 = new CarBean("BMW", "Black", 2);
+        carBean1.setCurrentSpeed(0);
         CarService carService1 = new CarService(carBean1);
-        carService1.accelerate(315);
+        carService1.isDriving();
+        carService1.accelerate(3);
+        System.out.println(carBean1);
+        carService1.isStopped();
+        carService1.canAccelerate();
+
+
 
         CarBean carBean2 = new CarBean("Volvo", "Grey metallic", 320);
         carBean2.setCurrentSpeed(20);
