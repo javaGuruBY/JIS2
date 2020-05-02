851115048e14e54b05886af39e4648807f00b0a6
Alexhorosko <Daldon772@gmail.com>
2020-05-02 23:55:07 +0300

    без Runnable!!!

--- homework/Aliaksandr_Horosko/Hw9_Task1_UniqueWordVocabulary/src/main/java/by/alhr/Hw9_Task1_UniqueWordVocabulary/Hw9Task1UniqueWordVocabularyDemo.java
+++ homework/Aliaksandr_Horosko/Hw9_Task1_UniqueWordVocabulary/src/main/java/by/alhr/Hw9_Task1_UniqueWordVocabulary/Hw9Task1UniqueWordVocabularyDemo.java
@@ -7,10 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Hw9Task1UniqueWordVocabularyDemo {
 
-	public static void main(String[] args) {
-		Thread thread = new Thread(new UniqueWordVocabulary());
-		thread.start();
-//		SpringApplication.run(Hw9Task1UniqueWordVocabularyDemo.class, args);
-	}
+    public static void main(String[] args) {
+        UniqueWordVocabulary uniqueWordVocabulary = new UniqueWordVocabulary();
+        uniqueWordVocabulary.addWord("Sasha");
+        uniqueWordVocabulary.addWord("Dima");
+        uniqueWordVocabulary.addWord("Oleg");
+        uniqueWordVocabulary.addWord("");
+        uniqueWordVocabulary.addWord("Olga");
+        uniqueWordVocabulary.addWord("Dima");
+        uniqueWordVocabulary.addWord("Sveta");
 
+        uniqueWordVocabulary.getWordsCount();
+        uniqueWordVocabulary.printVocabulary();
+    }
+//		SpringApplication.run(Hw9Task1UniqueWordVocabularyDemo.class, args);
 }
--- homework/Aliaksandr_Horosko/Hw9_Task1_UniqueWordVocabulary/src/main/java/by/alhr/Hw9_Task1_UniqueWordVocabulary/service/UniqueWordVocabulary.java
+++ homework/Aliaksandr_Horosko/Hw9_Task1_UniqueWordVocabulary/src/main/java/by/alhr/Hw9_Task1_UniqueWordVocabulary/service/UniqueWordVocabulary.java
@@ -3,7 +3,7 @@ package by.alhr.Hw9_Task1_UniqueWordVocabulary.service;
 import java.util.HashSet;
 import java.util.Set;
 
-public class UniqueWordVocabulary implements Runnable {
+public class UniqueWordVocabulary {
     public Set<String> vocabularySet = new HashSet<String>();
 
     public void addWord(String word) {
@@ -28,20 +28,5 @@ public class UniqueWordVocabulary implements Runnable {
         for (String collectionSoderjimoe : vocabularySet){
             System.out.println(collectionSoderjimoe);
         }
-    }
-
-    @Override
-    public void run() {
-        UniqueWordVocabulary uniqueWordVocabulary = new UniqueWordVocabulary();
-        uniqueWordVocabulary.addWord("Sasha");
-        uniqueWordVocabulary.addWord("Dima");
-        uniqueWordVocabulary.addWord("Oleg");
-        uniqueWordVocabulary.addWord("");
-        uniqueWordVocabulary.addWord("Olga");
-        uniqueWordVocabulary.addWord("Dima");
-        uniqueWordVocabulary.addWord("Sveta");
-
-        uniqueWordVocabulary.getWordsCount();
-        uniqueWordVocabulary.printVocabulary();
     }
 }
