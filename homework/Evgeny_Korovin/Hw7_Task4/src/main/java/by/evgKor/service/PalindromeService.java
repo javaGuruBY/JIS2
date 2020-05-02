3cb57149a37b8f20cbde8360ccd574de592939b9
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-05-02 15:22:12 +0300

    corrected Hw7 Task4

--- homework/Evgeny_Korovin/Hw7_Task4/src/main/java/by/evgKor/service/PalindromeService.java
+++ homework/Evgeny_Korovin/Hw7_Task4/src/main/java/by/evgKor/service/PalindromeService.java
@@ -4,11 +4,11 @@ public class PalindromeService implements Runnable {
     @Override
     public void run() {
         PalindromeService palindromeService=new PalindromeService();
-        palindromeService.isPalindrome("LEvel");
+        palindromeService.isPalindrome("LE,vel");
     }
 
     boolean isPalindrome(String text) {
-        text = text.replaceAll("[\\s]", "").toLowerCase();
+        text = text.replaceAll("[\\p{Punct}\\s]", "").toLowerCase();
         boolean result = true;
         for (int i = 0; i < text.length() / 2; i++) {
             if (text.charAt(i) != text.charAt(text.length() - i - 1)) {
